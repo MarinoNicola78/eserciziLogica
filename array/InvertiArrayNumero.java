37c13792128111830500055057329df0aa01ee7d
NicolaMarino <marino.nicola78@gmail.com>
2023-10-22 20:01:39 +0200

    esercizio InvertiArrayNumero

---
+++ array/InvertiArrayNumero.java
@@ -0,0 +1,18 @@
+package array;
+
+public class InvertiArrayNumero {
+
+	public static void main(String[] args) {
+
+		int[] numeri = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+		for (int i = numeri.length; i > 0; i--) {
+
+			System.out.print(numeri[i-1] + ", ");
+
+		}
+		
+
+	}
+
+}
