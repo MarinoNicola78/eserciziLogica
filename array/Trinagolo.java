24eb9d1c8281575068144a3bb819e1f3ea496356
NicolaMarino <marino.nicola78@gmail.com>
2023-10-23 13:36:37 +0200

    esercizio triangolo Array

---
+++ array/Trinagolo.java
@@ -0,0 +1,22 @@
+package array;
+
+public class Trinagolo {
+
+	public static void main(String[] args) {
+		
+		
+		int[] righa = {1,2,3,4,5,6,7,8,9,10};
+		//int[] colonna = {1,2,3,4,5,6,7,8,9,10};
+		
+		for (int i = 0; i < righa.length; i++) { // numero righe
+			
+			
+			for (int j = 0; j <= i; j++) // numero colonne
+				
+				System.out.print(righa[j]); // stampa la righa
+				System.out.println(); // a capo per iniziare altra righa
+			
+		}
+
+	}
+}
